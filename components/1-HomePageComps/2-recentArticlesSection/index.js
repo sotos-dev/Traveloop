0e58bc54b4aeb3c9eb3c04cb4bb3f42124d12c7e
SotoS <xsavannahx@protonmail.com>
2022-06-18 14:37:29 +0300

    recent articles homepage section done, responsive

--- components/1-HomePageComps/2-recentArticlesSection/index.js
+++ components/1-HomePageComps/2-recentArticlesSection/index.js
@@ -4,29 +4,54 @@ import styles from "./recentArticlesSection.module.scss"
 import FemaleTraveller from "../../../assets/images/travel.jpg"
 import { recentArticlesCardsData } from "../../../utils/HomepageUtils"
 import RecentArticlesCard from "./recentArticlesCard"
+import Link from "next/link"
 
 const RecentArticlesSection = () => {
   return (
     <>
       <section className={styles.recentArticlesSection}>
         <Container>
-          {/* Big Image */}
-          <div className={styles.bigImg}>
-            <Image
-              src={FemaleTraveller}
-              alt='female traveler in narrow old fashioned street'
-            />
-          </div>
-          {/* Card Links + Title */}
-          <div className={styles.content}>
-            <div className={styles.title}>
-              <p className={styles.overTitle}>RECENT ARTICLES</p>
-              <h2 className={styles.sectionTitle}>News & Stories</h2>
+          {/* RAS Wrapper */}
+          <div className={styles.rasWrapper}>
+            {/* Big Image */}
+            <div className={styles.bigImgWrapper}>
+              <Image
+                src={FemaleTraveller}
+                alt='female traveler in narrow old fashioned street'
+                layout='fill'
+                objectFit='cover'
+                className={styles.bigImg}
+              />
+              <div className={styles.bigImgShader}></div>
+              {/* Big Image Content */}
+              <div className={styles.bigImgContent}>
+                <p className={styles.bigImgDate}>April 8, 2022</p>
+                <p className={styles.bigImgTitle}>
+                  It's Easy to do better sustainable travel
+                </p>
+                <div className={styles.bigImgCategoryArea}>
+                  <Link href='/'>
+                    <a className={styles.bigImgCategories}>
+                      Travel inspiration
+                    </a>
+                  </Link>
+                  <Link href='/'>
+                    <a className={styles.bigImgCategories}>Stories</a>
+                  </Link>
+                </div>
+              </div>
             </div>
-            <div className={styles.cardLinks}>
-              {recentArticlesCardsData.map((card) => {
-                return <RecentArticlesCard key={card.title} card={card} />
-              })}
+            {/* Card Links + Title */}
+            <div className={styles.content}>
+              <div className={styles.title}>
+                <p className={styles.overTitle}>RECENT ARTICLES</p>
+                <h2 className={styles.sectionTitle}>News & Stories</h2>
+              </div>
+              <div className={styles.cardLinks}>
+                {recentArticlesCardsData.map((card) => {
+                  return <RecentArticlesCard key={card.title} card={card} />
+                })}
+              </div>
             </div>
           </div>
         </Container>
