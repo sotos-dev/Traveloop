462ebdbf54fed46cb516682789e4a8fcdab38c7d
SotoS <xsavannahx@protonmail.com>
2022-06-28 20:57:07 +0300

    started about page

---
+++ components/2-pagePlaceholder/1-heroSection/index.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import HeroImage from "../../../assets/images/about-hero.jpg"
+import styles from "./index.module.scss"
+
+const HeroSection = () => {
+  return (
+    <>
+      <section className={styles.heroSection}>
+        <div className={styles.imgWrapper}>
+          <Image
+            className={styles.img}
+            src={HeroImage}
+            alt='street with Havana old type 2 story houses'
+            layout='fill'
+            objectFit='cover'
+          />
+        </div>
+        <h1 className={styles.title}>Welcome to Traveloop</h1>
+      </section>
+    </>
+  )
+}
+
+export default HeroSection
---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import HeroSection from "../components/2-pagePlaceholder/1-heroSection"
+
+const About = () => {
+  return (
+    <>
+      {/* Hero Section */}
+      <HeroSection />
+      {/* Blog Writers */}
+
+      {/* About Traveloop */}
+
+      {/* Why Traveloop */}
+
+      {/* Get In Touch */}
+    </>
+  )
+}
+
+export default About
